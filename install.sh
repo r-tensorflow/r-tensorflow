48990df3135acd7c384849275e23cd60523b1d36
Daniel Falbel <dfalbel@gmail.com>
2019-10-14 16:25:39 -0300

    try changing the downloads data_dir

--- install.sh
+++ install.sh
@@ -22,7 +22,8 @@ Rscript -e 'tfds::install_tfds()'
 Rscript -e 'tfds::tfds_load(
   "imdb_reviews:1.0.0", 
   split = list("train[:60%]", "train[-40%:]", "test"), 
-  as_supervised = TRUE
+  as_supervised = TRUE,
+  data_dir = "tensorflow_datasets"
 )'
 
 
