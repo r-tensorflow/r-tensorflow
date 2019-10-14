bfeab9e23976b06f2dea1d25c585d657f1873e7e
Daniel Falbel <dfalbel@gmail.com>
2019-10-14 11:10:24 -0300

    build use an sh file to install deps

---
+++ install.sh
@@ -0,0 +1,9 @@
+Rscript -e 'install.packages(c("remotes", "blogdown"),  
+                             repos = "https://cran.rstudio.com")'
+Rscript -e 'remotes::install_github(c("rstudio/keras",
+                                      "rstudio/tensorflow",
+                                      "rstudio/reticulate",
+                                      "rstudio/tfdatasets",
+                                      "rstudio/tfhub",
+                                      "rstudio/tfds"))'
+Rscript -e 'tensorflow::install_tensorflow()'
