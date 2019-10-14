93416d08331d66be73949fd511b51a892b2c37ae
Daniel Falbel <dfalbel@gmail.com>
2019-10-14 15:01:57 -0300

    cache imdb review

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# Install R dependencies
+
 Rscript -e 'install.packages(c("remotes", "blogdown", "rprojroot", "pkgdown"),  
                              repos = "https://demo.rstudiopm.com/all/__linux__/bionic/latest")'
 Rscript -e 'remotes::install_github(c("rstudio/keras",
@@ -7,4 +9,20 @@ Rscript -e 'remotes::install_github(c("rstudio/keras",
                                       "rstudio/tfhub",
                                       "rstudio/tfds",
                                       "t-kalinowski/tfautograph"))'
-Rscript -e 'tensorflow::install_tensorflow(extra_packages = c("tensorflow_datasets", "tensorflow_hub"))'
+
+# Install Python dependencies
+
+Rscript -e 'tensorflow::install_tensorflow()'
+Rscript -e 'tensorflow::install_tfhub()'
+Rscript -e 'tensorflow::install_keras()'
+Rscript -e 'tensorflow::install_tfds()'
+
+# Download/cache used datasets
+
+Rscript -e 'tfds::tfds_load(
+  "imdb_reviews:1.0.0", 
+  split = list("train[:60%]", "train[-40%:]", "test"), 
+  as_supervised = TRUE
+)'
+
+
