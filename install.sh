2f50df745c6385248dea0af9c2a89cf9de289ec1
Daniel Falbel <dfalbel@gmail.com>
2019-10-14 14:03:03 -0300

    try again from rocker/verse

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-apt-get -y install libssl-dev
-
 Rscript -e 'install.packages(c("remotes", "blogdown", "rprojroot", "pkgdown"),  
                              repos = "https://demo.rstudiopm.com/all/__linux__/bionic/latest")'
 Rscript -e 'remotes::install_github(c("rstudio/keras",
