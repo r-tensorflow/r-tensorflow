2a89fa2473fecae239640db0696e91e5c0892cfa
Daniel Falbel <dfalbel@gmail.com>
2019-11-13 15:29:04 -0300

    automate all api reference generation

--- scripts/api.R
+++ scripts/api.R
@@ -1,41 +1,82 @@
 # Clone the package
-git2r::clone(
-  "https://github.com/rstudio/keras", 
-  local_path = path.expand("~/Desktop/packages")
+
+packages <- list(
+  keras = "rstudio/keras",
+  tensorflow = "rstudio/tensorflow",
+  tfdatasets = "rstudio/tfdatasets"
 )
 
-# Edit pkgdown config
-config <- yaml::yaml.load_file("~/Desktop/packages/pkgdown/_pkgdown.yml")
-config$template$path <- normalizePath(file.path("themes/rstudio-docs-theme/pkgdown/templates"))
-yaml::write_yaml(config, "~/Desktop/packages/pkgdown/_pkgdown.yml")
+download_source <- function(repo) {
+  # download the source
+  file <- remotes:::remote_download(remotes:::github_remote(repo))
+  exdir <- tempfile()
+  fs::dir_create(path = exdir)
+  # extracts the downloaded package a temporary directory
+  untar(file, exdir = exdir)
+  fs::dir_ls(exdir)
+}
 
-# Build reference
-unlink("~/Desktop/packages/website/reference/", recursive = TRUE)
-pkgdown::build_reference("~/Desktop/packages/", lazy = FALSE, preview = FALSE)
+modify_pkgdown_config <- function(exdir) {
+  pkg_yml <- file.path(exdir, "pkgdown", "_pkgdown.yml")
 
-# Copy reference to the website folder
-unlink("content/reference/keras/", recursive = TRUE)
-fs::dir_copy(
-  path = "~/Desktop/packages/website/reference/",
-  new_path = "content/reference/keras"
+  # Edit pkgdown config
+  config <- yaml::yaml.load_file(pkg_yml)
+  config$template$path <- normalizePath(file.path("themes/rstudio-docs-theme/pkgdown/templates"))
+  yaml::write_yaml(config, pkg_yml)
+}
+
+rebuild_reference <- function(exdir) {
+  ref <- file.path(exdir, "website", "reference")
+  # Build reference
+  unlink(ref, recursive = TRUE)
+  callr::r(function(exdir) {
+    pkgdown::build_reference(exdir, lazy = FALSE, preview = FALSE)
+  }, args = list(exdir = exdir))
+}
+
+copy_reference <- function(exdir, name) {
+  
+  # Copy reference to the website folder
+  ref <- paste0(file.path(getwd(), "content", "reference", name), "/")
+  unlink(ref, recursive = TRUE)
+  
+  Sys.sleep(1)
+  
+  reference <- paste0(file.path(exdir, "website", "reference"), "/")
+  if (!fs::is_dir(reference))
+    reference <- paste0(file.path(exdir, "docs", "reference"), "/")
+  
+  fs::dir_copy(
+    path = reference,
+    new_path = ref
   )
+  
+  file.rename(
+    from = file.path(ref, "index.html"),
+    to = file.path(ref, "_index.html")
+  )
+  
+  # rename all aliases from `.Rd` to `.html`.
+  # a bug in the whisker package doesnt allow us to add this to hugo templating.
+  fs::dir_ls(ref) %>% 
+    purrr::walk(function(path) {
+      lines <- readLines(path)
+      alias <- which(stringr::str_detect(lines, "aliases:"))
+      if (length(alias) == 1) {
+        lines[alias] <- stringr::str_replace_all(lines[alias], ".Rd", ".html")
+        writeLines(lines, path)
+      }
+    })
+  
+}
 
-file.rename(
-  from = "content/reference/keras/index.html", 
-  to = "content/reference/keras/_index.html"
-)
+purrr::iwalk(packages, function(repo, name) {
+  exdir <- download_source(repo)
+  modify_pkgdown_config(exdir)
+  rebuild_reference(exdir)
+  copy_reference(exdir, name)
+})
 
-# rename all aliases from `.Rd` to `.html`.
-# a bug in the whisker package doesnt allow us to add this to hugo templating.
-fs::dir_ls("content/reference/keras/") %>% 
-  purrr::walk(function(path) {
-    lines <- readLines(path)
-    alias <- which(stringr::str_detect(lines, "aliases:"))
-    if (length(alias) == 1) {
-      lines[alias] <- stringr::str_replace_all(lines[alias], ".Rd", ".html")
-      writeLines(lines, path)
-    }
-  })
 
 
 
